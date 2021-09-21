8da465d9d7d60b83382d98f82997948eb925b7be
edgarvilla <edgar.villa.perez@gmail.com>
2021-09-21 12:18:30 -0500

    Proyecto de Prueba

--- src/app/components/hola-mundo.components.ts
+++ src/app/components/hola-mundo.components.ts
@@ -2,7 +2,7 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'hola-mundo',
-    template: '<h1 class="text-center">Probando Amgular Filtro Posts</h1>'
+    template: '<h1 class="text-center">Probando Angular Filtro Posts</h1>'
 })
 export class HolaMundoComponent{
 
